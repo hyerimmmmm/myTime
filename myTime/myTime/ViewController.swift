68e7b497600eea6bff20bf825ca7bac4fb589b6d
hyerim <ax1021@naver.com>
2024-10-23 16:15:04 +0900

    [setting] 프로젝트 생성

---
+++ myTime/myTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myTime
+//
+//  Created by 김혜림 on 10/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
